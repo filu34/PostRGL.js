164215dfa1e2a929131d87f39b3dc86c1c315437
Filip Szettel <filu3@protonmail.com>
2021-07-05 22:16:49 +0000

    Added more letter models for future alphabet.

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
-import WebGLUtils from "./WebGL_Utilities/WebGLUtils.js";
-import Test_Draw from "./WebGL_Utilities/Test_Draw.js";
+import WebGLUtils	from "./WebGL_Utilities/WebGLUtils.js";
+import Test_Draw	from "./WebGL_Utilities/Test_Draw.js";
+import E_Letter		from "./geometries/E_Letter.js";
 
 function main () {
 
@@ -18,13 +19,13 @@ function main () {
 		0.9, -0.3 
         ];
 
-	const { Canvas, Context, Program } = Initialization ( Vertices, "2D", true ); 
+	const { Canvas, Context, Program } = Initialization ( E_Letter, "2D", true ); 
 
 
 	Resize_Canvas_To_Display_Size( Context. canvas );
 	Context. viewport ( 0, 0, Context.canvas.width, Context.canvas.height );
 
-	Test_Draw ( Vertices, "2D" );
+	Test_Draw ( E_Letter, "2D" );
 	console.log ( Context. canvas);
 
 
